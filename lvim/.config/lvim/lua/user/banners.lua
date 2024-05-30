361c0abcc94c3e61922547999d86d04f04f0a6f3
Joe Valente <joeyv1in100@gmail.com>
2024-05-29 23:01:01 -0500

    Custom icons for lvim menus

---
+++ lvim/.config/lvim/lua/user/banners.lua
@@ -0,0 +1,34 @@
+local M = {}
+M.dashboard = function()
+  local headers = {
+    [[                                  'ccc;.                                                                         ]],
+    [[                                     :l,lc,d.                                                                    ]],
+    [[                                     :l,c:,d.                                                                    ]],
+    [[                                     .olcc;.                                                                     ]],
+    [[                                  .':oc                                                                          ]],
+    [[                         ..',;cccc:oxxd:.         ;ooooooooooooooolc:,..                                         ]],
+    [[           .;;. ...,;:ccc:;,...   ;o.. 'cl'.      cOOOOOOOOOOOOOOOOOOOOko,.                                      ]],
+    [[          'xxxxl:;,'..          .lc.     .;l:.    cOOOOOOOOOOOOOOOOOOOOOOOx;                                     ]],
+    [[          .:odo.               .o;          'cl'. cOOOOOOc''''''',;cxOOOOOOOl.                                   ]],
+    [[             'd.              ,d'             .;l:;::dOOO;           ,xOOOOOOc ......            ......          ]],
+    [[              ;o.            :o.                 :xxd.xOO;            'kOOOOOk.:OOOOOc         .lOOOOx.          ]],
+    [[               cl          .lc.                  .;:;:kOO;             xOOOOOO. ;kOOOOc       .oOOOOx.           ]],
+    [[               .o:     .'''o;                     :kkOOOO;            .kOOOOOk.  ,kOOOOc     .oOOOOd.            ]],
+    [[                .d,  .cl:::o'                     cOOOOOO;           .oOOOOOOl    'kOOOOl.  .dOOOOo.             ]],
+    [[                 ;do:.x.dk::d::;,''....           :kOOOOO;       ..,ckOOOOOOd.     .xOOOOl .dOOOOo.              ]],
+    [[                 'oo: ;l::cd.....',,;:cccccc:;;,',c:,xOOOkkkkkkkkOOOOOOOOOkl.       .xOOOOldOOOOc                ]],
+    [[                        ...cl.              ....'lxx,lOOOOOOOOOOOOOOOOOOkc.          .dOOOOOOOk:                 ]],
+    [[                            ;o.                   ,coOOOOOOOOOOOOOOOOkc'.             .dOOOOOk;                  ]],
+    [[                             'd,                  cOOOOOO;.....;kOOOOOd.              .lOOOOk'                   ]],
+    [[                           .,l:lc.                cOOOOOO;      :OOOOOOd.            .oOOOOk'  .                 ]],
+    [[                         .:l,.  :o.               cOOOOOO;       lOOOOOOo.          .dOOOOx.  'd:                ]],
+    [[                       .cl'      'o,              cOOOOOO;        oOOOOOOo.        .dOOOOd.  ;xxxc.              ]],
+    [[                    .,l:.         .dl:.           cOOOOOO;        .dOOOOOOo.      .xOOOOo.  .dxxxxl.             ]],
+    [[                 ;llo;.           .oxd.           cOOOOOO;         .xOOOOOOl     'kOOOOl.    .oxxxxl.            ]],
+    [[                 cdo'               .             cOOOOOO;          .xOOOOOOl   ,kOOOOc       .oxxxxo.           ]],
+    [[                                                  cOOOOOO;           .kOOOOOOl :kOOOk:         .oxxxxd.          ]],
+    [[                                                  ,llllll'            'lllllll:lllll;           .cccccc.         ]],
+  }
+end
+
+return M
