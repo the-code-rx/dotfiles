e3aa70f637552b7a113622f6ce94ccbabd0a6b9d
Joe Valente <joeyv1in100@gmail.com>
2024-06-01 15:34:59 -0500

    Added DBUI and better integrated with LunarVim's existing menu

--- lvim/.config/lvim/lua/user/which-key.lua
+++ lvim/.config/lvim/lua/user/which-key.lua
@@ -1,35 +1,44 @@
--- Most of this which-key config was taken from https://github.com/IfCodingWereNatural/miminal-nvim
-local kind = require('user.kind')
-
+-- The base of this which-key config was taken from https://github.com/IfCodingWereNatural/minimal-nvim/tree/lunarvim
 local wk = lvim.builtin.which_key
 
-wk.mappings["a"] = { ":Alpha<cr>", kind.icons.screen .. " Dashboard" }
+-- DadBod UI Keymaps --
+wk.mappings["D"] = {
+  name = "DBUI",
+  b = { ":DBUI<cr>", " Open DBUI" },
+  t = { ":DBUIToggle<cr>", " Toggle DBUI" },
+  c = { ":DBUIClose<cr>", " Close DBUI" },
+  a = { ":BDUIAddConnection<cr>", " Add Connection" }
+}
 
+-- Persistence.nvim Keymaps --
 wk.mappings["S"] = {
-  name = " persistence.nvim",
-  s = { "<cmd>lua require('persistence').load()<cr>", kind.icons.clock .. " Reload last session for dir" },
-  l = { "<cmd>lua require('persistence').load({ last = true })<cr>", kind.icons.clock .. " Restore last session" },
-  Q = { "<cmd>lua require('persistence').stop()<cr>", kind.icons.exit .. " Quit without saving session" },
-}
--- wk.mappings["o"]["t"] = { ':ObsidianTemplate<cr>', kind.symbols_outline.File .. " Obsidian Templates" }
-
-wk.mappings["l"]["t"] = { ":LvimToggleFormatOnSave<cr>", kind.symbols_outline.File .. " Toggle format-on-save" }
-wk.mappings["l"]["R"] = { ":LspRestart<cr>", kind.icons.exit .. " Restart" }
-
-wk.mappings["s"]["w"] = {
-  "<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.expand('<cword>') })<cr>",
-  ' ' .. kind.cmp_kind.EnumMember .. " Search Word Under Cursor"
+  name = "Persistence.nvim",
+  s = { "<cmd>lua require('persistence').load()<cr>", "Reload last session for dir" },
+  l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
+  Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
 }
 
-wk.mappings["m"] = {
-  name = ' ' .. kind.todo_comments.PERF .. " Harpoon",
-  m = { ":lua require('harpoon.mark').add_file()<cr>", "Mark file" },
+-- Obsidian Keymaps --
+wk.mappings["o"] = {
+  name = "Obsidian",
+  t = { ':ObsidianTemplate<cr>', "Obsidian Templates" },
+  T = { ":ObsidianTags<cr>", "Obsidian Tags" },
+  b = { ":ObsidianBacklinks<cr>", "Obsidian Backlinks" },
+}
+
+-- LSP Restart added to LSP defaults for LunarVim --
+wk.mappings["lR"] = { ":LspRestart<cr>", "LSP Restart" }
+
+-- Harpoon Keymaps --
+wk.mappings["m"] = { ":lua require('harpoon.mark').add_file()<cr>", "Harpoon Mark File" }
+wk.mappings["h"] = {
+  name = "Harpoon",
   t = { ":lua require('harpoon.ui').toggle_quick_menu()<cr>", "Toggle UI" },
   a = { ":lua require('harpoon.ui').nav_file(1)<cr>", "Goto mark 1" },
   s = { ":lua require('harpoon.ui').nav_file(2)<cr>", "Goto mark 2" },
   d = { ":lua require('harpoon.ui').nav_file(3)<cr>", "Goto mark 3" },
   f = { ":lua require('harpoon.ui').nav_file(4)<cr>", "Goto mark 4" },
-  g = { ":lua require('harpoon.ui').nav_file(5)<cr>", "Goto mark 5" },
+  g = { ":lua require('harpon.ui').nav_file(5)<cr>", "Goto mark 5" },
   q = { ":lua require('harpoon.ui').nav_file(6)<cr>", "Goto mark 6" },
   w = { ":lua require('harpoon.ui').nav_file(7)<cr>", "Goto mark 7" },
   e = { ":lua require('harpoon.ui').nav_file(8)<cr>", "Goto mark 8" },
@@ -38,18 +47,28 @@ wk.mappings["m"] = {
   p = { ":lua require('harpoon.ui').nav_prev()<cr>", "Prev file" },
 }
 
-wk.mappings["W"] = {
-  name = ' ' .. kind.icons.screen .. " Window Ctrl",
-  h = { '<C-w>|', 'Maximize window horizontally (|)' },
-  v = { '<C-w>_', 'Maximize window vertically (_)' },
-  ['='] = { '<C-w>=', 'Resize windows equally' },
-  s = { ":lua require('telescope-tabs').list_tabs()<cr>", 'Search Tabs' },
-}
+-- Remapping noh from default of h no n --
+wk.mappings["n"] = { ":noh<cr>", "No Highlight" }
 
-wk.mappings['x'] = { ":xa<cr>", "Save All and Quit", }
-
-wk.mappings["t"] = {
-  name = ' Telescope',
-  p = { ':Telescope projects<cr>', 'Projects' }, -- requires telescope-project.nvim plugin
-  r = { ':Telescope resume<cr>', 'Resume' },
+-- Telescope Kemaps Added to defults already in LunarVim --
+wk.mappings['x'] = { ":xa<cr>", "Save All & Quit", }
+wk.mappings["sp"] = { ":Telescope projects<cr>", 'Projects' }
+wk.mappings["sB"] = { ":Telescope git_branches<cr>", 'Branches' }
+wk.mappings["sb"] = { ":Telescope buffers<cr>", 'Buffers' }
+wk.mappings["sw"] = {
+  "<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.expand('<cword>') })<cr>",
+  "Search Word Under Cursor"
 }
+wk.mappings["sc"] = { ":Telescope commands<cr>", "Commands" }
+wk.mappings["sC"] = { ":Telescope command_history<cr>", "Command History" }
+wk.mappings["sj"] = { ":Telescope jumplist", "Jumplist" }
+wk.mappings["sz"] = { "Telescope current_buffer_fuzzy_find", "fzf Search Current Buffer" }
+
+-- Remapping c to :bd as that is what I would expect it to do. --
+wk.mappings["c"] = { ":bd<cr>", "Buffer Delete" }
+
+-- Git Kemaps added to defaults for LunarVim --
+wk.mappings["gt"] = { ":Gitsigns toggle_current_line_blame<cr>", "Toggle Line Blame" }
+
+--Python virtual env swithcer --
+wk.mappings["lp"] = { "<cmd>lua require('swenv.api').pick_venv()<cr>", "Python Env" }
