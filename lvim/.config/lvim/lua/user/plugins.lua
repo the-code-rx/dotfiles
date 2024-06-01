002cbe5d468685068573a0443aa45ff40cc2f7f6
Joe Valente <joeyv1in100@gmail.com>
2024-06-01 18:28:28 -0500

    refactored with comments

--- lvim/.config/lvim/lua/user/plugins.lua
+++ lvim/.config/lvim/lua/user/plugins.lua
@@ -1,8 +1,24 @@
 lvim.plugins = {
   { "ThePrimeagen/vim-be-good" },
+  -- Navigation --
   { "ThePrimeagen/harpoon" },
-  { "ThePrimeagen/git-worktree.nvim" },
   { "stevearc/oil.nvim" },
+  -- Git Workfolow ---
+  { "ThePrimeagen/git-worktree.nvim" },
+  { "tpope/vim-fugitive" },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+  },
+  -- Documentation --
+  { "rmagatti/goto-preview" },
+  -- Notifcations --
   {
     "rcarriga/nvim-notify",
     config = function()
@@ -33,6 +49,8 @@ lvim.plugins = {
       "rcarriga/nvim-notify",
     }
   },
+  -- Writing and Distraction Free Coding --
+  { "/preservim/vim-pencil" },
   {
     "folke/twilight.nvim",
     opts = {
@@ -43,12 +61,13 @@ lvim.plugins = {
     opts = {
     }
   },
+  -- Bettter Comments --
   {
     "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
     }
   },
+  -- Restor Old Nvim Sessions --
   {
     "folke/persistence.nvim",
     event = "BufReadPre",
@@ -59,16 +78,16 @@ lvim.plugins = {
       })
     end
   },
+  -- View Hex/RGB Colors in Nvim --
   { "norcalli/nvim-colorizer.lua" },
+  -- Live Preview Markdown --
   {
     "iamcco/markdown-preview.nvim",
     config = function()
       vim.fn["mkdp#util#install"]()
     end
   },
-  { "szymonmaszke/vimpyter" },
-  { "AckslD/swenv.nvim" },
-  { "stevearc/dressing.nvim" },
+  -- Obsidian Notes --
   {
     "epwalsh/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
@@ -84,8 +103,6 @@ lvim.plugins = {
     dependencies = {
       -- Required.
       "nvim-lua/plenary.nvim",
-
-      -- see below for full list of optional dependencies 👇
     },
     opts = {
       workspaces = {
@@ -104,8 +121,20 @@ lvim.plugins = {
       -- see below for full list of options 👇
     },
   },
+  -- IMPROVED FORMATING --
+  -- Fold Lines --
+  {
+    'AckslD/nvim-trevJ.lua',
+    config = 'require("trevj").setup()',
+    init = function()
+      vim.keymap.set('n', '<leader>j', function()
+        require('trevj').format_at_cursor()
+      end)
+    end,
+  },
+  -- Use cs and ds to Change surround --
   { "tpope/vim-surround" },
-  { "tpope/vim-fugitive" },
+  -- Database Interface --
   { "kristijanhusak/vim-dadbod-ui" },
   { "kristijanhusak/vim-dadbod-completion" },
   {
@@ -125,20 +154,22 @@ lvim.plugins = {
       vim.g.db_ui_use_nerd_fonts = 1
     end,
   },
-  { "/preservim/vim-pencil" },
+  -- DATA SCIENCE TOOLS ---
+  -- qmd, ipynb, py, r, tex --
   { "goerz/jupytext.vim" },
   { "devOpifex/r.nvim" },
   { "quarto-dev/quarto-nvim" },
-  { "jmbuhr/otter.nvim" },
   { "jpalardy/vim-slime" },
   { "jamespeapen/Nvim-R" },
   { "ncm2/ncm2" },
   { "roxma/nvim-yarp" },
   { "gaalcaras/ncm-R" },
-  { "sirver/UltiSnips" },
-  { "ncm2/ncm2-ultisnips" },
   { "lervag/vimtex" },
   { "christoomey/vim-tmux-runner" },
+  -- Python Cond/Venv Management --
+  { "AckslD/swenv.nvim" },
+  { "stevearc/dressing.nvim" },
+  -- Navigate Between TMUX and NVIM --
   {
     "christoomey/vim-tmux-navigator",
     cmd = {
@@ -156,85 +187,10 @@ lvim.plugins = {
       { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
   },
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-  },
-  {
-    'AckslD/nvim-trevJ.lua',
-    config = 'require("trevj").setup()',
-    init = function()
-      vim.keymap.set('n', '<leader>j', function()
-        require('trevj').format_at_cursor()
-      end)
-    end,
-  },
-  {
-    'rmagatti/goto-preview',
-    config = function()
-      require('goto-preview').setup {
-        width = 110, -- Width of the floating window
-        height = 20, -- Height of the floating window
-        border = { "↖", "─", "┐", "│", "┘", "─", "└", "│" }, -- Border characters of the floating window
-        default_mappings = true,
-        debug = false, -- Print debug information
-        opacity = nil, -- 0-100 opacity level of the floating window where 100 is fully transparent.
-        resizing_mappings = false, -- Binds arrow keys to resizing the floating window.
-        post_open_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
-        references = { -- Configure the telescope UI for slowing the references cycling window.
-          telescope = require("telescope.themes").get_dropdown({ hide_preview = false })
-        },
-        -- These two configs can also be passed down to the goto-preview definition and implementation calls for one off "peak" functionality.
-        focus_on_open = true,                                         -- Focus the floating window when opening it.
-        dismiss_on_move = false,                                      -- Dismiss the floating window when moving the cursor.
-        force_close = true,                                           -- passed into vim.api.nvim_win_close's second argument. See :h nvim_win_close
-        bufhidden = "wipe",                                           -- the bufhidden option to set on the floating window. See :h bufhidden
-        stack_floating_preview_windows = true,                        -- Whether to nest floating windows
-        preview_window_title = { enable = true, position = "right" }, -- Whether
-      }
-    end
-  },
+  -- CMP Plugins added to Lunar Vim Base --
+  { "sirver/UltiSnips" },
+  { "jmbuhr/otter.nvim" },
+  { "jalvesaq/cmp-nvim-r" },
+  { "ncm2/ncm2-ultisnips" },
   -- Other plugins can be added here
 }
-
--- Noice cmd popup configuration
-require("noice").setup({
-  lsp = {
-    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-    },
-  },
-  -- you can enable a preset for easier configuration
-  presets = {
-    bottom_search = true,         -- use a classic bottom cmdline for search
-    command_palette = true,       -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false,       -- add a border to hover docs and signature help
-  },
-})
--- colorizer setup
-require 'colorizer'.setup()
-
--- Python virtual environment setup --
-require('swenv').setup({
-  -- Should return a list of tables with a `name` and a `path` entry each.
-  -- Gets the argument `venvs_path` set below.
-  -- By default just lists the entries in `venvs_path`.
-  get_venvs = function(venvs_path)
-    return require('swenv.api').get_venvs(venvs_path)
-  end,
-  -- Path passed to `get_venvs`.
-  venvs_path = vim.fn.expand('~/venvs'),
-  -- Something to do after setting an environment, for example call vim.cmd.LspRestart
-  post_set_venv = nil,
-})
