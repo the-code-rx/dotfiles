7d76eeef976b104fb659b90f7e97f6eebaa4bea3
Joe Valente <joeyv1in100@gmail.com>
2024-06-09 17:40:07 -0500

    initial commit

---
+++ nvim/.config/nvim/lua/plugins/tex.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "kdheepak/cmp-latex-symbols",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        { name = "latex_symbols", priority = 700 },
+      }))
+    end,
+  },
+  { -- preview equations
+    "jbyuki/nabla.nvim",
+    keys = {
+      { "<leader>ct", ':lua require("nabla").popup()<CR>', desc = "Toggle Math Equations" },
+    },
+  },
+}
