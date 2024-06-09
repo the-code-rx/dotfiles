d58c42b3d9ce28d8a74675477b325bc4e359a037
Joe Valente <joeyv1in100@gmail.com>
2024-06-09 17:34:19 -0500

    initial commit

---
+++ nvim/.config/nvim/ftplugin/sql.lua
@@ -0,0 +1,6 @@
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "sql", "mysql", "plsql" },
+  callback = function()
+    require('cmp').setup.buffer({ sources = { { name = 'vim-dadbod-completion' } } })
+  end,
+})
