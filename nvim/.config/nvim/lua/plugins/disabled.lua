b5070af19f919e608d60849e4281de184723cd35
Joe Valente <joev@teamunclejoe.org>
2024-08-30 17:49:47 -0500

    playing with configuration

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   { "echasnovski/mini.animate", enabled = false },
   { "R-nvim/R.nvim", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
